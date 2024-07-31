fc89b08432ed3944cc8b114692385f17d895a4e0
J0kerModZ <167721907+J0kerModZ@users.noreply.github.com>
2024-07-30 23:40:08 -0400

    v1

---
+++ MenuMods/Mods.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+
+
+namespace Raldis_Crack_House_Mod_Menu.MenuMods
+{
+    public class Mods : MonoBehaviour
+    {
+
+        public static void BaldiESP()
+        {
+            GameObject.Find("Game/Characters/Baldi/BaldiSprite").SetActive(false);
+            GameObject.Find("Game/Characters/Baldi/RaldiDrip").SetActive(true);
+            GameObject.Find("Game/Characters/Baldi/RaldiDrip").GetComponent<SpriteRenderer>().material.shader = Shader.Find("GUI/Text Shader");
+            GameObject.Find("Game/Characters/Baldi/RaldiDrip").GetComponent<SpriteRenderer>().material.color = Color.red;
+
+            GameObject gameObject = new GameObject("Line");
+            LineRenderer lineRenderer = gameObject.AddComponent<LineRenderer>();
+            Color red = Color.red;
+            lineRenderer.startColor = red;
+            lineRenderer.endColor = red;
+            lineRenderer.startWidth = 0.01f;
+            lineRenderer.endWidth = 0.01f;
+            lineRenderer.positionCount = 2;
+            lineRenderer.useWorldSpace = true;
+            lineRenderer.SetPosition(0, GameObject.Find("Game/Player/Player").transform.position);
+            lineRenderer.SetPosition(1, GameObject.Find("Game/Characters/Baldi/RaldiDrip").GetComponent<RealBillboard>().transform.position);
+            lineRenderer.material.shader = Shader.Find("GUI/Text Shader");
+            UnityEngine.Object.Destroy(lineRenderer, Time.deltaTime);
+            UnityEngine.Object.Destroy(gameObject, Time.deltaTime);
+        }
+
+        public static void Speedhack()
+        {
+            GameObject.Find("Game/Player/Player").GetComponent<PlayerScript>().walkSpeed = 60;
+            GameObject.Find("Game/Player/Player").GetComponent<PlayerScript>().runSpeed = 90;
+            GameObject.Find("Game/Player/Player").GetComponent<PlayerScript>().infiniteStamina = true;
+        }
+
+        public static void SpeedhackOff()
+        {
+            GameObject.Find("Game/Player/Player").GetComponent<PlayerScript>().walkSpeed = 20;
+            GameObject.Find("Game/Player/Player").GetComponent<PlayerScript>().runSpeed = 25;
+            GameObject.Find("Game/Player/Player").GetComponent<PlayerScript>().infiniteStamina = false;
+        }
+
+        public static void Infinite_Stamina()
+        {
+            GameObject.Find("Game/Player/Player").GetComponent<PlayerScript>().infiniteStamina = true;
+        }
+
+        public static void Infinite_StaminaOff()
+        {
+            GameObject.Find("Game/Player/Player").GetComponent<PlayerScript>().infiniteStamina = false;
+        }
+
+
+
+        public static void NoNPC()
+        {
+            GameObject.Find("Game/Characters").SetActive(false);
+        }
+
+        public static void YesNPC()
+        {
+            GameObject.Find("Game/Characters").SetActive(true);
+        }
+
+
+        public static void Blah()
+        {
+            NotebookScript[] array = Object.FindObjectsOfType<NotebookScript>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i].Ping();
+            }
+        }
+
+        public static void FuxkUI()
+        {
+            GameObject.Find("Game/UI/edpJumpscare").SetActive(false);
+            GameObject.Find("Game/UI/Hud/UIEvents").SetActive(false);
+        }
+
+        public static void LoveUI()
+        {
+            GameObject.Find("Game/UI/Hud/UIEvents").SetActive(true);
+        }
+
+        public static void Iphone() 
+        {
+            GameObject.Find("Game/UI/iPhoneSelectScreen").SetActive(true);
+        }
+
+        public static void _3rd_Preson() 
+        {
+            GameObject.Find("Game/Player/Player/DiarreahCamera").SetActive(true);
+            GameObject.Find("Game/Player/Player/DiarreahCamera").GetComponent<Camera>().fieldOfView = 120f;
+        }
+
+        public static void _3rd_PresonOff()
+        {
+            GameObject.Find("Game/Player/Player/DiarreahCamera").SetActive(false);
+        }
+
+        public static void MoneyHack()
+        {
+            GameObject.Find("Game/Controllers/MoneyManager").GetComponent<MoneyManager>().money = 9999f;
+            GameObject.Find("Game/Controllers/PlayerStats").GetComponent<PlayerStats>().data.cashInPocket = 9999f;
+        }
+    }
+}
---
+++ ModMenu/Menu.cs
@@ -0,0 +1,110 @@
+﻿using Raldis_Crack_House_Mod_Menu.MenuMods;
+using UnityEngine;
+using BepInEx;
+
+namespace Raldis_Crack_House_Mod_Menu.ModMenu
+{
+    [BepInPlugin("com.j0ker.modz.raldi", "J0kersRaldiMenu", "1.0.0")]
+    public class Menu : BaseUnityPlugin
+    {
+        private bool besp;
+        private Rect guiBoxRect = new Rect(0, 35, 245, 385);
+        private bool sh;
+        private bool inf;
+        private bool thirdp;
+
+        void OnGUI()
+        {
+            GUI.backgroundColor = Color.black;
+            GUI.color = Color.red;
+            guiBoxRect = GUI.Window(0, guiBoxRect, DragWindow, "J0kers RALDI Menu");
+
+        }
+
+        void DragWindow(int windowID)
+        {
+            GUI.color = Color.red;
+            GUI.backgroundColor = Color.black;
+
+            if (GUI.Button(new Rect(0f, 85f, 245f, 25f), "Quit Game"))
+            {
+                Application.Quit();
+            }
+
+            if (GUI.Button(new Rect(0f, 125f, 120f, 25f), "Raldi ESP"))
+            {
+                this.besp = !this.besp;
+            }
+            if (this.besp)
+            {
+                Mods.BaldiESP();
+            }
+
+            if (GUI.Button(new Rect(125f, 125f, 120f, 25f), "Ping Books [ESP]"))
+            {
+                Mods.Blah();
+            }
+
+            if (GUI.Button(new Rect(0f, 155f, 245f, 25f), "Speed Hack"))
+            {
+                this.sh = !this.sh;
+            }
+            if (this.sh)
+            {
+                Mods.Speedhack();
+            }
+            else
+            {
+                Mods.SpeedhackOff();
+            }
+
+            if (GUI.Button(new Rect(0f, 185f, 120f, 25f), "Inf Stamina"))
+            {
+                this.inf = !this.inf;
+            }
+            if (this.inf)
+            {
+                Mods.Infinite_Stamina();
+            }
+            else
+            {
+                Mods.Infinite_StaminaOff();
+            }
+
+            if (GUI.Button(new Rect(125f, 185f, 120f, 25f), "God Mode"))
+            {
+                Mods.FuxkUI();
+            }
+
+            if (GUI.Button(new Rect(0f, 225f, 245f, 25f), "Money Hack"))
+            {
+                Mods.MoneyHack();
+            }
+
+            if (GUI.Button(new Rect(0f, 255f, 120f, 25f), "Spawn Iphone"))
+            {
+                Mods.Iphone();
+            }
+
+            if (GUI.Button(new Rect(125f, 255f, 120f, 25f), "Look At Player"))
+            {
+                this.thirdp = !this.thirdp;
+            }
+            if (this.thirdp)
+            {
+                Mods._3rd_Preson();
+            }
+            else
+            {
+                Mods._3rd_PresonOff();
+            }
+
+            if (GUI.Button(new Rect(0f, 285f, 245f, 25f), "God Mode"))
+            {
+
+            }
+
+            GUI.DragWindow();
+        }
+    }
+}
